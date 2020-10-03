fdcdc330b181edb805ce2380bb86bdc44d958743
ecorun <ecorun@gmail.com>
2020-10-03 19:56:06 +0100

    updates

--- resources/views/livewire/manager/not-a-manager.blade.php
+++ resources/views/livewire/manager/not-a-manager.blade.php
@@ -1,12 +1,12 @@
 <div>
-    <div class="py-4 bg-gray-100 px-4">
-        <p class="text-lg bg-white px-4 px-4 mb-4 break-long-words font-medium">
+    <div class="bg-gray px-4 py-4">
+        <p class="text-lg bg-white mb-4 break-long-words font-medium">
             some random text about who a manager is and the abilities of a manager... I just want to make this long
             some random text about who a manager is and the abilities of a manager... I just want to make this long
             some random text about who a manager is and the abilities of a manager... I just want to make this long
             some random text about who a manager is and the abilities of a manager... I just want to make this long
         </p>
-        <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        <div class="grid bg-gray-100 grid-cols-1 sm:grid-cols-3 gap-4">
             <div class="flex justify-center items-center">
                 <img style="height: 100%; width: 100%;"
                     src="{{ asset('/storage/app-images/remote-business-manager.jpg') }}" />
