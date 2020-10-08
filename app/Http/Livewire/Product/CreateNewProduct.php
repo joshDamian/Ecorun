0414d9062a5b4a09204bd649e7eed70f3ed5ee86
ecorun <ecorun@gmail.com>
2020-10-09 00:11:06 +0100

    updates

--- app/Http/Livewire/Product/CreateNewProduct.php
+++ app/Http/Livewire/Product/CreateNewProduct.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Product;
 
 use App\Models\Enterprise;
+use Intervention\Image\Facades\Image;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -16,9 +17,10 @@ class CreateNewProduct extends Component
     public $description;
     public $available_stock;
     public $photos = [];
+    public $product;
 
     protected $rules = [
-        'photos.*' => ['required', 'image', 'max:3072'],
+        'photos.*' => ['required', 'image', 'max:4096'],
         'name' => ['required', 'min:4', 'string'],
         'description' => ['required', 'min:20'],
         'available_stock' => ['required', 'int', 'min:1'],
@@ -29,16 +31,35 @@ class CreateNewProduct extends Component
     {
         $this->validate();
 
-        $this->enterprise()
+        $this->product = $this->enterprise()
             ->products()
             ->create([
-                'name' => $this->name
+                'name' => $this->name,
+                'description' => $this->discription,
             ]);
+
+        foreach ($this->photos as $photo) {
+            $photo_path = $photo->store('product-photos', 'public');
+            $photo = Image::make(public_path("/storage/{$photo_path}"))->fit(1024, 1024);
+            $photo->save();
+
+            $this->product->gallery()->create([
+                'image_url' => $photo_path,
+                'label' => 'product_image'
+            ]);
+        }
     }
 
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
+    }
+
+    public function updatedPhoto()
+    {
+        $this->validate([
+            'photos.*' => ['required', 'image', 'max:3072']
+        ]);
     }
 
     public function render()
