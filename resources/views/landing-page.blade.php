6a0bb93cfb56d9f363ae02eface6d43d43fc71cb
ecorun <ecorun@gmail.com>
2020-11-20 02:38:47 +0100

    updates 20-11-20

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -31,7 +31,6 @@
 </head>
 
 <body class="font-sans leading-normal tracking-normal">
-
     <div x-data="{ visible: null }" x-cloak>
         <nav class="bg-white sticky top-0 border-b-4 border-blue-800">
             <div>
@@ -40,13 +39,13 @@
                     <li class="text-right">
                         <div x-show.transition="visible" class="flex flex-wrap">
                             <a class="mr-4" href="/login">
-                                <x-jet-button class="bg-blue-700">
+                                <x-jet-button class="bg-blue-600">
                                     Login
                                 </x-jet-button>
                             </a>
 
                             <a href="/register">
-                                <x-jet-button class="bg-blue-600">
+                                <x-jet-button class="bg-blue-800">
                                     Signup
                                 </x-jet-button>
                             </a>
@@ -60,8 +59,8 @@
             <div class="bg-gray-200">
                 <div class="grid grid-cols-1 sm:p-3 md:p-10 md:grid-cols-2">
                     <div class="">
-                        <h3 class="text-3xl font-medium text-gray-800 px-3 sm:px-0 sm:pb-3 sm:py-0 py-2">
-                            Connect with people, buy products, build and manage businesses.
+                        <h3 class="text-2xl sm:text-3xl font-semibold text-gray-800 px-3 sm:px-0 sm:pb-3 sm:py-0 py-2">
+                            Connect with people, shop, build and manage businesses.
                         </h3>
 
                         <h3 class="text-lg font-medium text-gray-600 px-3 sm:px-0 sm:pb-4 pb-3 sm:py-0">
@@ -85,21 +84,21 @@
 
                     <div class="p-3 md:p-6 justify-center flex items-center">
                         <div class="flex uppercase">
-                            <div class="text-xl md:text-3xl font-semibold text-blue-800">
+                            <div class="text-xl sm:text-3xl font-semibold text-blue-800">
                                 <span class="mr-3">connect</span>
                                 <span class="mr-3">
                                     <i class="fas text-blue-800 fa-exchange-alt"></i>
                                 </span>
                             </div>
 
-                            <div class="text-xl md:text-3xl font-semibold text-blue-600">
-                                <span class="mr-3">buy</span>
+                            <div class="text-xl sm:text-3xl font-semibold text-blue-600">
+                                <span class="mr-3">shop</span>
                                 <span class="mr-3">
-                                   <i class="fas text-blue-800 fa-exchange-alt"></i>
+                                    <i class="fas text-blue-800 fa-exchange-alt"></i>
                                 </span>
                             </div>
 
-                            <div class="text-xl md:text-3xl font-semibold text-blue-800">
+                            <div class="text-xl sm:text-3xl font-semibold text-blue-800">
                                 <span>build</span>
                             </div>
                         </div>
@@ -107,36 +106,36 @@
                 </div>
             </div>
 
-            <div class="grid grid-cols-1 sm:grid-cols-3 bg-gray-100 sm:p-3 gap-2 sm:gap-3 md:gap-10 md:p-10">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 bg-gray-100 sm:p-3 gap-2 sm:gap-3 md:gap-9 md:p-9">
                 <div class="bg-white sm:bg-transparent">
                     <div class="p-2 flex justify-center">
-                        <span class="fa-stack fa-4x">
+                        <span class="fa-stack fa-3x">
                             <i class="fas fa-circle text-blue-800 fa-stack-2x"></i>
                             <i class="fa-stack-1x text-white fas fa-user-friends"></i>
                         </span>
                     </div>
 
-                    <div class="p-3 border-gray-400 sm:rounded-lg sm:border text-gray-600">
+                    <div class="p-3 border-gray-200 sm:shadow sm:rounded-lg sm:border text-gray-600">
                         <h3 class="text-xl font-semibold text-blue-800">
-                            Connect
+                            Connect with people
                         </h3>
                         <div class="px-5 py-2">
                             <ul class="list-disc text-blue-800">
                                 <li>
                                     <span class="text-gray-700">
-                                        Build an online presence for your business.
+                                        Make new friends.
                                     </span>
                                 </li>
 
                                 <li>
                                     <span class="text-gray-700">
-                                        Unite fun and business.
+                                        Follow amazing trends.
                                     </span>
                                 </li>
 
                                 <li>
                                     <span class="text-gray-700">
-                                        Interact with your customers.
+                                        Join & initiate interesting conversations.
                                     </span>
                                 </li>
                             </ul>
@@ -154,33 +153,34 @@
 
                 <div class="bg-white sm:bg-transparent">
                     <div class="p-2 flex justify-center">
-                        <span class="fa-stack fa-4x">
+                        <span class="fa-stack fa-3x">
                             <i class="fas fa-circle text-blue-600 fa-stack-2x"></i>
-                            <span class="fa-stack-1x text-white font-semibold">&#8358;</span>
+                            <i class="fas fa-shopping-cart text-white fa-stack-1x"></i>
+                            {{-- <span class="fa-stack-1x text-white font-semibold">&#8358;</span> --}}
                         </span>
                     </div>
 
-                    <div class="p-3 border-gray-400 sm:rounded-lg sm:border text-gray-600">
+                    <div class="p-3 border-gray-200 sm:shadow sm:rounded-lg sm:border text-gray-600">
                         <h3 class="text-xl font-semibold text-blue-600">
-                            Buy
+                            Shop
                         </h3>
                         <div class="px-5 py-2">
                             <ul class="list-disc text-blue-600">
                                 <li>
                                     <span class="text-gray-700">
-                                        Build an online presence for your business.
+                                        Shop products and services.
                                     </span>
                                 </li>
 
                                 <li>
                                     <span class="text-gray-700">
-                                        Unite fun and business.
+                                        Easy shopping with a cart system.
                                     </span>
                                 </li>
 
                                 <li>
                                     <span class="text-gray-700">
-                                        Interact with your customers.
+                                        Advertise used products for sale.
                                     </span>
                                 </li>
                             </ul>
@@ -198,13 +198,13 @@
 
                 <div class="bg-white sm:bg-transparent">
                     <div class="p-2 flex justify-center">
-                        <span class="fa-stack fa-4x">
+                        <span class="fa-stack fa-3x">
                             <i class="fas fa-circle text-blue-800 fa-stack-2x"></i>
                             <i class="fa-stack-1x text-white fas fa-warehouse"></i>
                         </span>
                     </div>
 
-                    <div class="p-3  border-gray-400 sm:rounded-lg text-gray-600 sm:border">
+                    <div class="p-3 border-gray-200 sm:shadow sm:rounded-lg text-gray-600 sm:border">
                         <h3 class="text-xl font-semibold text-blue-800">
                             Build and manage businesses
                         </h3>
@@ -230,7 +230,13 @@
 
                                 <li>
                                     <span class="text-gray-700">
-                                        Leverage on an inventory system for product management.
+                                        Leverage on a virtual warehouse for product management.
+                                    </span>
+                                </li>
+
+                                <li>
+                                    <span class="text-gray-700">
+                                        Deploy a team to manage your business.
                                     </span>
                                 </li>
                             </ul>
