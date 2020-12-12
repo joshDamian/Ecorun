d8dae040d61d5bef0e82c29a0df94d303efe4534
joshDamian <akachijc@gmail.com>
2020-12-12 21:18:14 +0100

    build updates

--- resources/views/guest-landing-page.blade.php
+++ resources/views/guest-landing-page.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/webfonts.css') }}">
+
     {{--  <link href="https://afeld.github.io/emoji-css/emo7ji.css" rel="stylesheet">
     <style>
         [x-cloak] {
