a43c646e61209e46e2909f839b7beee82c2a7f5d
ecorun <ecorun@gmail.com>
2021-01-23 18:42:04 +0100

    nothing

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,5 @@
 <svg class="w-full h-full">
-    <rect x="50" y="20" rx="20" ry="20" width="100" height="100"
+    <rect x="50" y="20" rx="20" ry="20" width="150" height="200"
         style="fill:rgb(14, 14, 187);stroke: White;stroke-width:5;opacity:2.5" />
-    <text x="80" y="150" font-size="160" font-family='Redressed' fill="#ffffff">E</text>
+    <text x="80" y="150" font-size="80" font-family='Redressed' fill="#ffffff">E</text>
 </svg>
