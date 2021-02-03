6baad8c4c177022a997a918086099b6640b4e7b3
joshDamian <akachijc@gmail.com>
2021-02-03 02:56:41 +0100

    no major fixes

--- app/Http/Livewire/Traits/MultipleImageSelector.php
+++ app/Http/Livewire/Traits/MultipleImageSelector.php
@@ -5,7 +5,7 @@ namespace App\Http\Livewire\Traits;
 trait MultipleImageSelector
 {
     public $addedImages = [];
-    protected array $image_validation =  [
+    protected array $image_validation = [
         'bail',
         'mimes:jpeg,bmp,png,svg,webp',
         'file',
@@ -13,25 +13,28 @@ trait MultipleImageSelector
         'max:10240'
     ];
 
-    public function updatedAddedImages()
-    {
+    public function updatedAddedImages() {
         $this->photos = collect($this->photos)->merge($this->addedImages)->all();
         return;
     }
 
-    public function removeFromPhotos($key)
-    {
+    public function removeFromPhotos($key) {
         unset($this->photos[(int) $key]);
         return;
     }
 
-    public function validPreviewUrl($photo, $key)
-    {
+    public function validPreviewUrl($photo) {
         try {
             return $photo->temporaryUrl();
         } catch (\Throwable $th) {
-            $this->removeFromPhotos($key);
             return null;
         }
+    }
+
+    public function getValidPhotosProperty() {
+        $this->photos = collect($this->photos)->filter(function($photo) {
+            return $this->validPreviewUrl($photo);
+        })->values()->all();
+        return $this->photos;
     }
 }
--- resources/views/components/connect/image/multiple-selector.blade.php
+++ resources/views/components/connect/image/multiple-selector.blade.php
@@ -2,11 +2,7 @@
 
 <div>
     <div class="grid grid-cols-3 gap-2">
-        @foreach ($photos as $key => $photo)
-        @php
-        $photo_url = $this->validPreviewUrl($photo, $key);
-        @endphp
-        @if($photo_url)
+        @foreach ($this->valid_photos as $key => $photo)
         <div>
             <div class="text-right">
                 <x-jet-secondary-button wire:click="removeFromPhotos({{$key}})" class="p-0 text-red-600">
@@ -15,13 +11,12 @@
             </div>
             <div class="p-1 border border-gray-300">
                 <div id="selected_{{$key}}" wire:ignore
-                    style="background-image: url('{{ $photo_url }}'); background-size: cover; background-position: center center;"
+                    style="background-image: url('{{ $photo->temporaryUrl() }}'); background-size: cover; background-position: center center;"
                     class="w-full h-20 sm:h-36">
                 </div>
             </div>
             <x-jet-input-error for="photos.{{$key}}" />
         </div>
-        @endif
         @endforeach
         <div class="flex items-end">
             <x-jet-secondary-button x-on:click="$refs.addedPhotos.click()" class="text-blue-700">
@@ -30,5 +25,5 @@
         </div>
     </div>
     <input name="addedImages" class="hidden" x-ref="addedPhotos" accept="image/*" type="file" wire:model="addedImages"
-        multiple />
+    multiple />
 </div>
