02448b551d568970b9551a8f3d045460bb4644a9
joshDamian <akachijc@gmail.com>
2021-02-03 04:39:18 +0100

    no major fixes

--- app/Http/Livewire/BuildAndManage/Product/CreateNewProduct.php
+++ app/Http/Livewire/BuildAndManage/Product/CreateNewProduct.php
@@ -10,27 +10,30 @@ use Livewire\Component;
 
 class CreateNewProduct extends Component
 {
-    use UploadPhotos, MultipleImageSelector;
+    use UploadPhotos,
+    MultipleImageSelector;
 
     public $business;
     public $photos = [];
     public $product_created = false;
-    public $product = ['_name', '_description', '_price', '_available stock'];
+    public $product = ['_name',
+        '_description',
+        '_price',
+        '_available stock'];
     public $business_is_store;
 
-    public function create()
-    {
+    public function create() {
         $this->validate();
 
         $this->product = $this->business
-            ->products()->create(
-                [
-                    'name' => ucwords(strtolower($this->product['_name'])),
-                    'description' => $this->product['_description'],
-                    'price' => $this->product['_price'],
-                    'available_stock' => $this->product['_available stock'],
-                ]
-            );
+        ->products()->create(
+            [
+                'name' => ucwords(strtolower($this->product['_name'])),
+                'description' => $this->product['_description'],
+                'price' => $this->product['_price'],
+                'available_stock' => $this->product['_available stock'],
+            ]
+        );
 
         /*  Category::find($this->product_category->title)->products()->save($this->product);
         */
@@ -43,38 +46,35 @@ class CreateNewProduct extends Component
         return [
             'photos.*' => $this->image_validation,
             'photos' => 'required',
-            'product._name' => ['required', 'min:4', 'string'],
-            'product._description' => ['required', 'min:20'],
+            'product._name' => ['required',
+                'min:4',
+                'string'],
+            'product._description' => ['required',
+                'min:20'],
             'product._available stock' => ($this->business_is_store || (!empty($this->product['_available stock'])))
-                ? ['required', 'int', 'min:1'] : '',
-            'product._price' => ['required', 'int', 'min:1'],
+            ? ['required',
+                'int',
+                'min:1'] : '',
+            'product._price' => ['required',
+                'int',
+                'min:1'],
         ];
     }
 
-    public function updated($propertyName)
-    {
+    public function updated($propertyName) {
         $this->validateOnly($propertyName);
     }
 
-    public function updatedPhotos(): void
-    {
-        $this->validate([
-            'photos.*' => ['image', 'max:10240']
-        ]);
-    }
 
-    public function resetData()
-    {
+    public function resetData() {
         return $this->reset('product_created', 'product', 'photos');
     }
 
-    public function mount()
-    {
+    public function mount() {
         $this->business_is_store = $this->business->isStore();
     }
 
-    public function render()
-    {
+    public function render() {
         return view('livewire.build-and-manage.product.create-new-product');
     }
 }
