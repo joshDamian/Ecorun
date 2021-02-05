f6e9679260e4a1a2b82ebde812ae5a6a1201bcde
joshDamian <akachijc@gmail.com>
2021-02-05 16:51:26 +0100

    build updates pre final launch

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -48,7 +48,7 @@
                 </div>
 
                 @if($post->content)
-                <x-display-text-content class="px-3 pt-3 rm-p-bottom-gap sm:px-5" :content="$post->safe_html" />
+                <x-display-text-content class="px-3 py-3 sm:px-5" :content="$post->safe_html" />
                 @endif
 
                 @if($image_count > 0)
