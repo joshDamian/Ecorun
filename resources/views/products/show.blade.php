5f69bf8568675b6540b01e0f6d9442059d4db869
joshDamian <akachijc@gmail.com>
2021-02-22 12:48:10 +0100

    added product bookmarking

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -35,7 +35,7 @@
 
                             <!-- Rating and link to reviews -->
                             <div class="flex flex-wrap items-baseline justify-between">
-                                <div class="mr-3 sm:mr-5">
+                                {{--    <div class="mr-3 sm:mr-5">
                                     <i class="text-blue-800 fa fa-star"></i>
                                     <i class="text-blue-800 fa fa-star"></i>
                                     <i class="text-blue-800 fa fa-star"></i>
@@ -47,11 +47,11 @@
                                         reviews (10)
                                     </a>
                                 </div>
+                                --}}
 
-                                <div class="text-black">
-                                    <i class="fas fa-heart"></i> save to wishlist
-                                </div>
+                                @livewire('connect.product.bookmark-product', ['product' => $product], key("product_bookmark_{$product->id}"))
                             </div>
+
 
                             <div class="flex flex-wrap items-baseline justify-between py-3 md:py-6">
                                 <!-- Price -->
